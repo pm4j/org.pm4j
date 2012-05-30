56f9df14ad002cdbf64a057865fc1e550d7a6a75
Olaf Boede <olaf.boede@gmx.de>
2012-05-30 16:25:39 +0200

    Removed redundant getMaxLen signature declaration.

--- pm4j-core/main/java/org/pm4j/core/pm/PmAttrNumber.java
+++ pm4j-core/main/java/org/pm4j/core/pm/PmAttrNumber.java
@@ -11,12 +11,5 @@ public interface PmAttrNumber<T extends Number> extends PmAttr<T> {
    * @return The minimum value for this attribute.
    */
   T getMin();
-  
-  /**
-   * FIXME olaf: should be an interface for all attributes that may be represented as text (--> PmAttrBase).
-   * 
-   * @return The maximum field length for representation as string.
-   */
-  int getMaxLen();
 
 }
