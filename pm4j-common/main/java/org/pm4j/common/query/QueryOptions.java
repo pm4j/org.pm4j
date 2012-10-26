424dc9d294c7c3e6606ce8bc287a80d956f3fbed
OBOEDE <OBOEDE@hamnw3904.hsdg-ad.int>
2012-10-26 16:05:31 +0200

    javadoc

--- pm4j-common/main/java/org/pm4j/common/query/QueryOptions.java
+++ pm4j-common/main/java/org/pm4j/common/query/QueryOptions.java
@@ -10,9 +10,30 @@ import java.util.List;
  */
 public interface QueryOptions {
 
+  /**
+   * Provides the sort order for the given attribute.
+   * <p>
+   * Is usually used by a column that asks if it is sortable.
+   *
+   * @param attrName name
+   * @return the corresponding sort order definition or <code>null</code>.
+   */
   SortOrder getSortOrder(String attrName);
+
+  /**
+   * Provides the default sort order.
+   *
+   * @return the default sort order or <code>null</code> is none is defined.
+   */
   SortOrder getDefaultSortOrder();
 
+  /**
+   * Provides the set of available compare definitions.
+   * <p>
+   * Usually this is used to provide the filter options the user may configure.
+   *
+   * @return the filter definition. Returns never <code>null</code>.
+   */
   List<FilterCompareDefinition> getCompareDefinitions();
 
 }
