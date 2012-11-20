85e1ef5b64215c35ae4377ff0d73b8c6a90449c2
Ramazan Kunas <ramazan@kunas.eu>
2012-11-20 21:59:09 +0100

    Mavenizing pm4j-facelets

--- pm4j-common/main/java/org/pm4j/common/selection/ItemIdConverter.java
+++
@@ -1,7 +0,0 @@
-package org.pm4j.common.selection;
-
-public interface ItemIdConverter<T_ITEM, T_ID> {
-
-  T_ID getIdForItem(T_ITEM item);
-  T_ITEM getItemForId(T_ID id);
-}
--- pm4j-common/main/java/org/pm4j/common/util/beanproperty/PropertyAndVetoableChangeListener.java
+++
@@ -1,8 +0,0 @@
-package org.pm4j.common.util.beanproperty;
-
-import java.beans.PropertyChangeListener;
-import java.beans.VetoableChangeListener;
-
-public interface PropertyAndVetoableChangeListener extends PropertyChangeListener, VetoableChangeListener {
-
-}
