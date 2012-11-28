20bb4a08d416d6cf75271e2bc378a0415b8517c9
OBOEDE <OBOEDE@hamnw3904.hsdg-ad.int>
2012-11-28 09:15:54 +0100

    ItemNavigator: in memory implementation

---
+++ pm4j-common/src/main/java/org/pm4j/common/pageable/inmem/ItemNavigatorInMem.java
@@ -0,0 +1,54 @@
+package org.pm4j.common.pageable.inmem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pm4j.common.pageable.ItemNavigator;
+import org.pm4j.common.selection.Selection;
+
+/**
+ * A navigator that keeps a collection of selected items in memory.
+ *
+ * @param <T> handled item type.
+ *
+ * @author olaf boede
+ */
+public class ItemNavigatorInMem<T> implements ItemNavigator<T> {
+
+  private List<T> items = new ArrayList<T>();
+  private int currentItemIdx;
+
+  public ItemNavigatorInMem(Selection<T> selection) {
+    if (selection != null) {
+      for (T t : selection) {
+        items.add(t);
+      }
+    }
+  }
+
+  @Override
+  public T navigateTo(int itemPos) {
+    currentItemIdx = Math.min(itemPos, getNumOfItems());
+    currentItemIdx = Math.max(currentItemIdx, 0);
+
+    return getCurrentItem();
+  }
+
+  @Override
+  public T getCurrentItem() {
+    return items.isEmpty() ? null : items.get(currentItemIdx);
+  }
+
+  @Override
+  public int getNumOfItems() {
+    return items.size();
+  }
+
+  @Override
+  public int getCurrentItemIdx() {
+    return currentItemIdx;
+  }
+
+
+
+}
