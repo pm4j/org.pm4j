a646e6cd2eec17b8c8ca7a30952266d31e96236c
OBOEDE <OBOEDE@HAMNW3904>
2012-12-17 19:13:38 +0100

    javadoc

--- pm4j-common/src/main/java/org/pm4j/common/selection/ItemIdConverter.java
+++ pm4j-common/src/main/java/org/pm4j/common/selection/ItemIdConverter.java
@@ -1,7 +1,24 @@
 package org.pm4j.common.selection;
 
+/**
+ * Converts between query or selection items and their corresponding identifiers.
+ *
+ * @param <T_ITEM> the item type.
+ * @param <T_ID> the item id type.
+ *
+ * @author olaf boede
+ */
 public interface ItemIdConverter<T_ITEM, T_ID> {
 
+  /**
+   * @param item the item to get the identifier for.
+   * @return the item identifier.
+   */
   T_ID getIdForItem(T_ITEM item);
+
+  /**
+   * @param id the identifier to get the item for.
+   * @return the item.
+   */
   T_ITEM getItemForId(T_ID id);
 }
