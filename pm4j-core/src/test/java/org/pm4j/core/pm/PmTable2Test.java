d9643a202044c8faa7498c0c4d3b8989fa747161
OBOEDE <OBOEDE@hamnw3904.hsdg-ad.int>
2013-02-08 16:13:36 +0100

    PmTableImpl2: added support for clearing cached table data through PmCacheApi

--- pm4j-core/src/test/java/org/pm4j/core/pm/PmTable2Test.java
+++ pm4j-core/src/test/java/org/pm4j/core/pm/PmTable2Test.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.pm4j.common.pageable.PageableCollectionUtil2;
 import org.pm4j.core.pm.annotation.PmBeanCfg;
 import org.pm4j.core.pm.annotation.PmFactoryCfg;
+import org.pm4j.core.pm.api.PmCacheApi;
 import org.pm4j.core.pm.impl.PmAttrIntegerImpl;
 import org.pm4j.core.pm.impl.PmAttrStringImpl;
 import org.pm4j.core.pm.impl.PmConversationImpl;
@@ -31,14 +34,48 @@ public class PmTable2Test {
         new RowBean("c", "a 'c'", 3)
     ));
 
-    myTablePm = new TablePm(new PmConversationImpl());
-    PageablePmBeanCollection<RowPm, RowBean> pc = new PageablePmBeanCollection<RowPm, RowBean>(myTablePm, RowPm.class, editedRowBeanList);
-    myTablePm.setPmPageableCollection(pc);
+    myTablePm = new TablePm(new PmConversationImpl()) {
+      /** We use here a simple in-memory data table.
+       * The table represents the items of the collection provided by this method. */
+      @Override
+      protected Collection<RowBean> getPmInMemoryCollectionImpl() {
+        return editedRowBeanList;
+      }
+    };
   }
 
   @Test
   public void testTable() {
     assertEquals(3, myTablePm.getTotalNumOfPmRows());
+    assertEquals("[a, b]", myTablePm.getRowPms().toString());
+  }
+
+  @Test
+  public void testDisplayBeanListChanges() {
+    assertEquals(3, myTablePm.getTotalNumOfPmRows());
+    assertEquals("[a, b]", myTablePm.getRowPms().toString());
+
+    // add an item to the represented list.
+    editedRowBeanList.add(new RowBean("d", "d", 4));
+    assertEquals("Number of items is cached. Thus the change will not be reflected automatically",
+                 3, myTablePm.getTotalNumOfPmRows());
+
+    PmCacheApi.clearPmCache(myTablePm);
+    assertEquals("After clearing the PM value caches we see the data change.",
+                 4, myTablePm.getTotalNumOfPmRows());
+    assertEquals("First page content is not changed.", "[a, b]", myTablePm.getRowPms().toString());
+    PageableCollectionUtil2.navigateToLastPage(myTablePm.getPmPageableCollection());
+    assertEquals("After a page switch the new item is visible.", "[c, d]", myTablePm.getRowPms().toString());
+
+    // remove an item on the current page.
+    editedRowBeanList.remove(2);
+    assertEquals("[a, b, d]", editedRowBeanList.toString());
+    assertEquals("Rerender the current page. It's not changed because the current page items are cached.",
+                 "[c, d]", myTablePm.getRowPms().toString());
+
+    PmCacheApi.clearPmCache(myTablePm);
+    assertEquals("After an update call the table should display the current content.",
+        "[d]", myTablePm.getRowPms().toString());
   }
 
   @Test
@@ -61,8 +98,11 @@ public class PmTable2Test {
     /** A column with a filter annotation that defines . */
     public final PmTableCol2 counter = new PmTableColImpl2(this);
 
-    /** ctor */
-    public TablePm(PmObject pmParent) { super(pmParent); }
+    /** Defines a page size of two items. */
+    public TablePm(PmObject pmParent) {
+      super(pmParent);
+      setNumOfPageRowPms(2);
+    }
   }
 
   @PmBeanCfg(beanClass=RowBean.class)
@@ -70,6 +110,12 @@ public class PmTable2Test {
     public final PmAttrString name = new PmAttrStringImpl(this);
     public final PmAttrString description = new PmAttrStringImpl(this);
     public final PmAttrInteger counter = new PmAttrIntegerImpl(this);
+
+    /** for debugging */
+    @Override
+    public String toString() {
+      return getPmBean().toString();
+    }
   }
 
   public static class RowBean {
@@ -81,6 +127,12 @@ public class PmTable2Test {
       this.name = name;
       this.description = description;
       this.counter = counter;
+    }
+
+    /** for debugging */
+    @Override
+    public String toString() {
+      return name;
     }
   }
 
