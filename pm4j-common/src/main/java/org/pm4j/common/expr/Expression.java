f003adb6527ecc24e1f28e21ca578c912d62557f
Olaf Boede <olaf.boede@gmx.de>
2013-03-06 11:20:44 +0100

    javadoc

--- pm4j-common/src/main/java/org/pm4j/common/expr/Expression.java
+++ pm4j-common/src/main/java/org/pm4j/common/expr/Expression.java
@@ -11,18 +11,24 @@ public interface Expression {
   public enum SyntaxVersion {
     /**
      * The (o) modifier has the same meaning than the (x) modifier.
-     * Named objects can be accessed without '#' sign. 
+     * Named objects can be accessed without '#' sign.
      */
-    VERSION_1, 
-    
+    VERSION_1,
+
     /**
-     * The (o) and (x) modifier have different meanings. 
+     * The (o) and (x) modifier have different meanings.
      * The # hash prefix is mandatory for named objects.
      */
     VERSION_2};
 
+  /**
+   * Executes the expression and returns the evaluated result value.
+   *
+   * @param ctxt the expression execution context.
+   * @return the evaluation result.
+   */
   Object exec(ExprExecCtxt ctxt);
-  
+
   /**
    * Value assignment operation. Is supported by path expressions that reference
    * attributes that provide public modification access (setter or public
