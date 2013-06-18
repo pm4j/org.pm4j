ed351af56389e7b0bb66f65296501770b8de5777
Olaf Boede <Olaf.Boede-External@hamburgsud.com>
2013-06-18 19:20:54 +0200

    PageableInMemCollection: removed unused sort method.

--- pm4j-common/src/main/java/org/pm4j/common/pageable/inmem/PageableInMemCollection.java
+++ pm4j-common/src/main/java/org/pm4j/common/pageable/inmem/PageableInMemCollection.java
@@ -1,7 +1,5 @@
 package org.pm4j.common.pageable.inmem;
 
-import java.util.Comparator;
-
 import org.pm4j.common.pageable.PageableCollection2;
 
 /**
@@ -13,12 +11,5 @@ import org.pm4j.common.pageable.PageableCollection2;
  * @author olaf boede
  */
 public interface PageableInMemCollection<T_ITEM> extends PageableCollection2<T_ITEM> {
-
-  /**
-   * Changes a sort order based on the given comparator.
-   *
-   * @param comparator the new comparator. May be <code>null</code> to switch back to the default sort order.
-   */
-  void setSortOrderComparator(Comparator<T_ITEM> comparator);
 
 }
