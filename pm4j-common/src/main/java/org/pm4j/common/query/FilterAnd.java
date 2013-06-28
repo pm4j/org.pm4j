d885d1d81a86170b98e810138a3c92760758be15
Olaf Boede <Olaf.Boede-External@hamburgsud.com>
2013-06-28 10:21:48 +0200

    FilterAnd: added a static join method

--- pm4j-common/src/main/java/org/pm4j/common/query/FilterAnd.java
+++ pm4j-common/src/main/java/org/pm4j/common/query/FilterAnd.java
@@ -20,6 +20,22 @@ public class FilterAnd implements FilterExpression {
   private static final long serialVersionUID = 1L;
   private List<FilterExpression> expressions;
 
+  /**
+   * A static helper method that joins the given set of expressions to an and-combined expression.
+   *
+   * @param expressions a set of expressions. May be empty or <code>null</code>.
+   * @return the resulting expression. May be <code>null</code> if the given parameter was empty or <code>null</code>.
+   */
+  public static FilterExpression joinToAnd(Collection<? extends FilterExpression> expressions) {
+    if (expressions == null || expressions.isEmpty()) {
+      return null;
+    } else if (expressions.size() == 1) {
+      return expressions.iterator().next();
+    } else {
+      return new FilterAnd(expressions);
+    }
+  }
+
   public FilterAnd(FilterExpression... expressions) {
     this(Arrays.asList(expressions));
   }
