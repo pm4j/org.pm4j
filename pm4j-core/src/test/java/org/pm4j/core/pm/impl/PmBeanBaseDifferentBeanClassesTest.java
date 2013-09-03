4250a967eb376f66e4d7cfab08f7fb28279b7223
Olaf Boede <Olaf.Boede-External@hamburgsud.com>
2013-09-03 17:40:55 +0200

    PmBeanBase: disabled assignability type check to keep some strange application code alive for some days.

--- pm4j-core/src/test/java/org/pm4j/core/pm/impl/PmBeanBaseDifferentBeanClassesTest.java
+++ pm4j-core/src/test/java/org/pm4j/core/pm/impl/PmBeanBaseDifferentBeanClassesTest.java
@@ -2,18 +2,19 @@ package org.pm4j.core.pm.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.pm4j.core.exception.PmRuntimeException;
 import org.pm4j.core.pm.PmObject;
 import org.pm4j.core.pm.annotation.PmBeanCfg;
 
 /**
- * 
+ *
  * Test using @PmBeanCfg and generic parameter in parallel, but different bean
  * classes, which are not assignable. This test must fail.
- * 
+ *
  * @author okossak
- * 
+ *
  */
 public class PmBeanBaseDifferentBeanClassesTest {
 
@@ -21,6 +22,7 @@ public class PmBeanBaseDifferentBeanClassesTest {
   DomainObjectImpl domainObject1 = new DomainObjectImpl();
 
   @Test(expected = PmRuntimeException.class)
+  @Ignore("FIXME oboede: see PmBeanBase ln. 383")
   public void testPmBeanImpl() {
     assertEquals("bean class must be defined by annotation", DomainObjectImpl.class, domainObjectPm.getPmBeanClass());
   }
