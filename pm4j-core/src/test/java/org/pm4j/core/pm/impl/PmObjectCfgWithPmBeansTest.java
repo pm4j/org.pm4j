aaacb42cfbe5b4037c713770e2bbcbb2309f9d34
Jakub Hetmanski <Jakub.Hetmanski-External@hamburgsud.com>
2015-01-30 16:01:12 +0100

    PmObjectCfg additional test + testing @PmObjectCfg(visible = Visible.IF_NOT_EMPTY)  influence on PmBeans

---
+++ pm4j-core/src/test/java/org/pm4j/core/pm/impl/PmObjectCfgWithPmBeansTest.java
@@ -0,0 +1,67 @@
+package org.pm4j.core.pm.impl;
+
+import org.junit.Test;
+import org.pm4j.core.pm.PmBean;
+import org.pm4j.core.pm.annotation.PmBeanCfg;
+import org.pm4j.core.pm.annotation.PmCacheCfg2;
+import org.pm4j.core.pm.annotation.PmCacheCfg2.Cache;
+import org.pm4j.core.pm.annotation.PmCacheCfg2.CacheMode;
+import org.pm4j.core.pm.annotation.PmObjectCfg;
+import org.pm4j.core.pm.annotation.PmObjectCfg.Visible;
+import org.pm4j.core.pm.api.PmCacheApi.CacheKind;
+import org.pm4j.tools.test.PmAssert;
+
+/**
+ * Test @PmObjectCfg annotation effects on PmBeans
+ * 
+ * @author JHETMANS
+ * 
+ */
+public class PmObjectCfgWithPmBeansTest {
+  
+  @Test
+  public void testBeanPmImpl2WithoutValue() {
+    TestPm testPm = new TestPm();
+    
+    PmAssert.assertVisible(testPm.beanPm);   
+  }
+  
+  @Test
+  public void testBeanPmImplWithValue() {
+    TestPm testPm = new TestPm();
+    
+    PmAssert.assertNotVisible(testPm.pmBeanImpl);   
+  }
+  
+  @Test
+  public void testBeanPmImplWithoutValue() {
+    TestPm testPm = new TestPm();
+    
+    testPm.pmBeanImpl.setPmBean(new Bean());
+    
+    PmAssert.assertVisible(testPm.pmBeanImpl);   
+  }
+  
+  private static class TestPm extends PmConversationImpl {
+    private boolean isReadOnly = false;
+    
+    @Override
+    protected boolean isPmReadonlyImpl() {
+      return isReadOnly;
+    }
+    
+    @PmBeanCfg(beanClass = Bean.class, autoCreateBean = true)
+    @PmObjectCfg(visible = Visible.IF_NOT_EMPTY)
+    @PmCacheCfg2(value = @Cache(property = CacheKind.VALUE, mode = CacheMode.ON))
+    public final PmBeanImpl2<Bean> beanPm = new PmBeanImpl2<Bean>(this);
+    
+    @PmBeanCfg(beanClass = Bean.class)
+    @PmObjectCfg(visible = Visible.IF_NOT_EMPTY)
+    public final PmBean<Bean> pmBeanImpl = new PmBeanImpl<Bean>(this);
+  }
+  
+  public static class Bean {
+    public Bean() {
+    }
+  }
+}
