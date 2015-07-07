65d0de3979aa9d9172d98e7835909a9d9ee11e11
Olaf Boede <Olaf.Boede-External@hamburgsud.com>
2015-07-07 17:53:03 +0200

    PmTabSetImpl: added unit tests

--- pm4j-core/src/test/java/org/pm4j/core/pm/PmTabSetTest.java
+++ pm4j-core/src/test/java/org/pm4j/core/pm/PmTabSetTest.java
@@ -1,24 +1,25 @@
 package org.pm4j.core.pm;
 
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.pm4j.tools.test._PmAssert.assertNoMessagesInConversation;
+
 import org.junit.Test;
+import org.pm4j.core.pm.api.PmEventApi;
 import org.pm4j.core.pm.impl.PmConversationImpl;
 import org.pm4j.core.pm.impl.PmObjectBase;
 import org.pm4j.core.pm.impl.PmTabSetImpl;
 
-import static org.junit.Assert.assertEquals;
-
+/**
+ * Tests for {@link PmTabSetImpl}.
+ *
+ * @author Olaf Boede
+ */
 public class PmTabSetTest {
 
-  private MyTabSet myTabSet;
-  private MyCommandDecorator myCommandDecorator;
-
-  @Before
-  public void setUp() {
-    PmConversation converation = new PmConversationImpl();
-    myTabSet = new MyTabSet(converation);
-    myCommandDecorator = new MyCommandDecorator();
-  }
+  private MyTabSet myTabSet = new MyTabSet(new PmConversationImpl());
+  private MyCommandDecorator myCommandDecorator = new MyCommandDecorator();
 
   @Test
   public void testTabSwitchCommandDecoratorForSwitchTab1ToTab2() {
@@ -59,6 +60,63 @@ public class PmTabSetTest {
     assertEquals(true, myCommandDecorator.afterDoWasCalled);
   }
 
+  @Test
+  public void testAllTabsDisabled() {
+    myTabSet.tab1.tabEnabled = false;
+    myTabSet.tab2.tabEnabled = false;
+    myTabSet.tab3.tabEnabled = false;
+
+    assertEquals("If there is no active tab, the first (inactive) tab will be the current tab.",
+                 myTabSet.tab1, myTabSet.getCurrentTabPm());
+
+    assertFalse("Switch to inactive tab shouldn't be possible.",
+                myTabSet.switchToTabPm(myTabSet.tab2));
+    assertNoMessagesInConversation(myTabSet);
+  }
+
+  @Test
+  public void testFirstTabDisabled() {
+    myTabSet.tab1.tabEnabled = false;
+
+    assertEquals("If the first tab is disabled, the second one will be the initial current tab.",
+                 myTabSet.tab2, myTabSet.getCurrentTabPm());
+
+    assertFalse("Switch to inactive tab shouldn't be possible.",
+                myTabSet.switchToTabPm(myTabSet.tab1));
+    assertTrue("Switch to active tab should be possible.",
+        myTabSet.switchToTabPm(myTabSet.tab3));
+    assertNoMessagesInConversation(myTabSet);
+  }
+
+  @Test
+  public void testLeaveDisabledTabOnResetCurrentTabCall() {
+    assertEquals(myTabSet.tab1, myTabSet.getCurrentTabPm());
+    myTabSet.tab1.tabEnabled = false;
+
+    myTabSet.resetCurrentTabPmIfInactive();
+    assertEquals(myTabSet.tab2, myTabSet.getCurrentTabPm());
+    assertNoMessagesInConversation(myTabSet);
+  }
+
+  @Test
+  public void testLeaveDisabledTabOnAllChangedEvent() {
+    assertEquals(myTabSet.tab1, myTabSet.getCurrentTabPm());
+    myTabSet.tab1.tabEnabled = false;
+
+    PmEventApi.broadcastPmEvent(myTabSet, PmEvent.ALL_CHANGE_EVENTS);
+
+    assertEquals(myTabSet.tab2, myTabSet.getCurrentTabPm());
+    assertNoMessagesInConversation(myTabSet);
+  }
+
+  @Test
+  public void testGetTabIndex() {
+    assertEquals(0, myTabSet.getTabIndex(myTabSet.tab1));
+    assertEquals(1, myTabSet.getTabIndex(myTabSet.tab2));
+    assertEquals(2, myTabSet.getTabIndex(myTabSet.tab3));
+  }
+
+
 
   static class MyCommandDecorator implements PmCommandDecorator {
 
@@ -95,8 +153,15 @@ public class PmTabSetTest {
   /** Each instance that wants to be handled as a tab needs to implement
    * the interface {@link PmTab}. */
   public static class MyTab extends PmObjectBase implements PmTab {
+    boolean tabEnabled = true;
+
     public MyTab(PmObject pmParent) {
       super(pmParent);
+    }
+
+    @Override
+    protected boolean isPmEnabledImpl() {
+      return tabEnabled;
     }
   }
 
